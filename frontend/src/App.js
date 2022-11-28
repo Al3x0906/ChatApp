2b2dbf0a573555cf630e27e2a65adb3a9bcd6150
Praval <pravalchauhan0906@gmail.com>
2022-11-28 21:41:03 +0530

    Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-import "./App.css";
-import Login from "./Login/login.js";
+import Login from "./components/Login/login.js";
 import ReactDOM from "react-dom";
-import React from "react";
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import React from "react";
+import "./app.css";
+import NavBar from "./components/Navbar/navbar";
+import SignUp from "./components/Signup/signup.js";
+function App() {
   return (
     <>
-      <Login />
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
