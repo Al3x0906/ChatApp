6f6f65d256607aeefc9263d50ec39ab5b7a511ae
Praval <pravalchauhan0906@gmail.com>
2022-11-28 22:13:58 +0530

    Login css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,17 @@ import React from "react";
 import "./app.css";
 import NavBar from "./components/Navbar/navbar";
 import SignUp from "./components/Signup/signup.js";
+import "./Login-Form-Dark.css"
 function App() {
   return (
-    <>
+    <div className="login-dark">
       <NavBar />
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
